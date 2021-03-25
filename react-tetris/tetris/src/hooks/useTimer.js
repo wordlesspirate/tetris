b2a608694abbad24e7879e8f504d7cc2fb3e2ab9
natalie.m.kendrick@gmail.com <natalie.m.kendrick@gmail.com>
2021-03-24 21:13:03 -0500

    laps added

--- react-tetris/tetris/src/hooks/useTimer.js
+++ react-tetris/tetris/src/hooks/useTimer.js
@@ -19,4 +19,36 @@ export const useTimer = () => {
 
   return [isRunning, setIsRunning, elapsedTime, setElapsedTime];
 
-}
+};
+
+export const useStopWatch = () => {
+
+  const [laps, setLaps] = useState([]); //may not need this
+  const [isRunning, setIsRunning, elapsedTime, setElapsedTime] = useTimer();
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+    setLap([]); //may not need this
+  };
+
+  //may not need this 
+  const handleAddLap = () => {
+    const prevTotal = 
+    laps.length > 0 ? laps.reduce((acc, curr) => acc + curr, 0) : 0;
+
+    const currentLap = laps.length > 0 ? elapsedTime - prevTotal : elapsedTime;
+
+    isRunning && setLaps([...laps, currentLap]);
+  };
+
+  return {
+    elapsedTime: elapsedTime.toFixed(1), laps,
+    addLaps: () => handleAddLap(),
+    resetTimer: () => handleReset(),
+    startTime: () => setIsRunning(true),
+    stopTimer: () => setIsRunning(false),
+    isRunning
+  };
+
+};
